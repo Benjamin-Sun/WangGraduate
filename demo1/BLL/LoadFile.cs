d86069440da8f64ea4ee0037213cee3ef7a634de
东 孙 <912249151@qq.com>
2019-04-08 01:03:26 +0800

    init

---
+++ demo1/BLL/LoadFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1.BLL
+{
+    public class LoadFile
+    {
+        public List<string> Load(string path)
+        {
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            string line;
+            List<string> list = new List<string>();
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line.ToString());
+                list.Add(line.ToString());
+            }
+            if (list == null)
+            {
+                return null;
+            }
+            else
+            {
+                Console.WriteLine(list);
+                return list;
+            }
+        }
+    }
+}
---
+++ demo1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using demo1.BLL;
+
+namespace demo1
+{
+    public partial class Form1 : Form
+    {
+        private LoadFile loadFile = new LoadFile();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            string path = ((Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            textBox1.Text = path;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = loadFile.Load(this.textBox1.Text);
+        }
+    }
+}
